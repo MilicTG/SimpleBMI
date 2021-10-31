bba5ff274ac8ca1b0099f1cf1102459c385156c6
MilicTG <milic.tg@gmail.com>
2021-10-31 17:55:19 +0100

    Initial commit

---
+++ app/src/main/java/dev/milic/simplebmi/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.milic.simplebmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.milic.simplebmi.ui.theme.SimpleBMITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleBMITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SimpleBMITheme {
+        Greeting("Android")
+    }
+}
