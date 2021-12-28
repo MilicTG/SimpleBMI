7b6eef00145d65a09e398f4b048672b657c8c1e4
MilicTG <milic.tg@gmail.com>
2021-12-28 10:33:22 +0100

    Update Constants.kt

--- app/src/main/java/dev/milic/simplebmi/util/Constants.kt
+++ app/src/main/java/dev/milic/simplebmi/util/Constants.kt
@@ -8,6 +8,9 @@ object Constants {
     const val SPLASH_GRAPH_ROUTE = "splash"
     const val HOME_GRAPH_ROUTE = "home"
 
+    //dataStore
+    const val BMI_DATASTORE = "bmi_datastore"
+
     //misc
     const val SPLASH_SCREEN_DELAY = 1000L
     const val DEV_NAME = "Ivan Milic"
