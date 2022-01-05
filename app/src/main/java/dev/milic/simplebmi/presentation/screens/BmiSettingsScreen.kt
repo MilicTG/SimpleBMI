02ef3bcf7e4f55d3d67b97dcfa40653a9d41d799
Ivan Milic <milic.tg@gmail.com>
2022-01-05 11:32:36 +0100

    Ready for first release

--- app/src/main/java/dev/milic/simplebmi/presentation/screens/BmiSettingsScreen.kt
+++ app/src/main/java/dev/milic/simplebmi/presentation/screens/BmiSettingsScreen.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Draw
 import androidx.compose.material.icons.outlined.Smartphone
-import androidx.compose.material.icons.outlined.Thermostat
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -54,11 +52,12 @@ fun BmiSettingsScreen(
                     .height(LARGE_PADDING)
             )
 
-            BmiSettingsOptionsRow(
-                rowTitle = "Change units",
-                icon = Icons.Outlined.Thermostat,
-                onRowClicked = { openUnitDialog.value = it }
-            )
+            //In Plan
+//            BmiSettingsOptionsRow(
+//                rowTitle = "Change units",
+//                icon = Icons.Outlined.Thermostat,
+//                onRowClicked = { openUnitDialog.value = it }
+//            )
 
             BmiSettingsOptionsRow(
                 rowTitle = "About app",
--- app/src/main/java/dev/milic/simplebmi/presentation/screens/BmiSplashScreen.kt
+++ app/src/main/java/dev/milic/simplebmi/presentation/screens/BmiSplashScreen.kt
@@ -4,20 +4,23 @@ import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.milic.simplebmi.domain.util.Constants.HOME_GRAPH_ROUTE
 import dev.milic.simplebmi.domain.util.Constants.SPLASH_SCREEN_DELAY
 import kotlinx.coroutines.delay
+import dev.milic.simplebmi.R
 
 @Composable
 fun BmiSplashScreen(
@@ -35,7 +38,6 @@ fun BmiSplashScreen(
 
     LaunchedEffect(key1 = true) {
         startAnimation = true
-        Log.d("ovde", "Jasta je")
         delay(SPLASH_SCREEN_DELAY)
         navController.navigate(HOME_GRAPH_ROUTE) {
             navController.popBackStack()
@@ -48,11 +50,11 @@ fun BmiSplashScreen(
             .background(MaterialTheme.colors.background),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Splash",
-            style = TextStyle(
-                fontSize = MaterialTheme.typography.h3.fontSize,
-            )
-        )
+       Image(
+           painter = painterResource(id = R.drawable.scale),
+           contentDescription = "App icon",
+           modifier = Modifier
+               .size(120.dp)
+       )
     }
 }
